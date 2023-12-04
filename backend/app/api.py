ed1dc7b8b14ef9b8eed0d743a6ac5ac10c6fa213
javiErUT2 <22393158@utcancun.edu.mx>
2023-12-04 12:47:47 -0500

    hola

--- backend/app/api.py
+++ backend/app/api.py
@@ -1,13 +1,11 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
-
 
 app = FastAPI()
 origins = [
     "http://localhost:3000",
     "localhost:3000"
 ]
-
 
 app.add_middleware(
     CORSMiddleware,
@@ -17,31 +15,29 @@ app.add_middleware(
     allow_headers=["*"]
 )
 
-# GET ROUTE
-@app.get("/", tags=["root"])
-async def read_root() -> dict:
-    return {"message": "Hello, World!"}
-
 todos = [
     {
-        "id": "1",
+        "id": 1,
         "item": "Read Stephen King's Cujo"
     },
     {
-        "id": "2",
+        "id": 2,
         "item": "Go for shopping"
     },
     {
-        "id": "3",
+        "id": 3,
         "item": "Cook dinner"
     }
 ]
+
+@app.get("/", tags=["root"])
+async def read_root() -> dict:
+    return {"message": "Hello, World!"}
 
 @app.get("/todo", tags=["todos"])
 async def get_todos() -> dict:
     return { "data": todos }
 
-# POST ROUTE
 @app.post("/todo", tags=["todos"])
 async def add_todo(todo: dict) -> dict:
     todos.append(todo)
@@ -49,7 +45,6 @@ async def add_todo(todo: dict) -> dict:
         "data": { "Todo added." }
     }
 
-# PUT ROUTE
 @app.put("/todo/{id}", tags=["todos"])
 async def update_todo(id: int, body: dict) -> dict:
     for todo in todos:
@@ -63,16 +58,18 @@ async def update_todo(id: int, body: dict) -> dict:
         "data": f"Todo with id {id} not found."
     }
     
-# DELETE ROUTE
 @app.delete("/todo/{id}", tags=["todos"])
 async def delete_todo(id: int) -> dict:
-    for todo in todos:
-        if int(todo["id"]) == id:
-            todos.remove(todo)
-            return {
-                "data": f"Todo with id {id} has been removed."
-            }
+    global todos
+    initial_length = len(todos)
 
-    return {
-        "data": f"Todo with id {id} not found."
-    }
+    todos = [todo for todo in todos if int(todo["id"]) != id]
+
+    if len(todos) < initial_length:
+        return {
+            "data": f"Todo with id {id} has been removed."
+        }
+    else:
+        return {
+            "data": f"Todo with id {id} not found."
+        }
